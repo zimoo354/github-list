eeb7b95dd777398227dc5126d85c0d30d2c1f59b
Carlos Isaias Ruiz <zimoo354@gmail.com>
2020-07-16 23:43:22 -0500

    Building interface (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Row, Col, Typography, Layout, Input, Button, Avatar, Card, List, Divider } from 'antd';
+import { Affix, Row, Col, Typography, Layout, Input, Button, Avatar, Card, List, Divider } from 'antd';
 import { getUser } from './api';
 
 const { Header, Content } = Layout;
@@ -22,19 +22,53 @@ const App = () => {
   };
 
   return (
-    <Layout style={{height:"100vh"}}>
-      <Header>
-        <div className="logo">
-          <Typography style={{ color: "white" }}>Github Finder</Typography>
-        </div>
-      </Header>
-      <Content>
+    <Layout style={{height:"auto"}}>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
         <Row>
-          <Col span={24}>
-            <Card style={{ height: "90vh" }}>
-            
-            </Card>          
+          <Col span={16}>
+            <Typography style={{ color: "white" }}>Github Finder</Typography>
           </Col>
+          <Col span={8}>
+            <Search placeholder="enter github username here..." onSearch={search} />
+          </Col>          
+        </Row>
+      </Header>
+      <Content style={{ background: "white" }}>
+        <Row>
+          {
+            user && repos && (
+              <>
+          <Col span={6}>
+            <Affix offsetTop={64}>
+              <Card title={user.login} extra={<a href={user.html_url} target="_blank">view profile</a>} style={{ height: "100%" }}>
+                <Avatar size={120} style={{ display: "block", margin: "0 auto 24px auto" }} src={user.avatar_url} />
+                <Meta
+                  title={user.login}
+                  description={user.bio}
+                />
+              </Card>  
+            </Affix>      
+          </Col>
+          <Col span={18} style={{ padding: "0 24px" }}>
+            <List
+              className="demo-loadmore-list"
+              itemLayout="horizontal"
+              dataSource={repos}
+              renderItem={item => (
+                <List.Item
+                  actions={[<a key="list-loadmore-edit" href={item.svn_url}>view</a>]}
+                >
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item.name}</a>}
+                    description={item.description}
+                  />
+                </List.Item>
+              )}
+            />                  
+          </Col>
+              </>
+            )
+          }
         </Row>
       </Content>
     </Layout>
