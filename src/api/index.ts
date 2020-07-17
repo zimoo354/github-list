ae907b7e2ba471d3a276915595473eabd7f0838b
Carlos Isaias Ruiz <zimoo354@gmail.com>
2020-07-16 23:43:38 -0500

    api profile and repos promises

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,13 @@
 const baseUrl = "https://api.github.com";
 
-export const getUserProfile = username => {
-  let user;
-  return fetch(`${baseUrl}/users/${username}`)
-    .then(response => response.json());
-};
-
 export const getUser = username => {
   const urls = [
     `${baseUrl}/users/${username}`,
     `${baseUrl}/users/${username}/repos`,
   ];
-  return Promise.all(urls.map(u=>fetch(u))).then(responses =>
-      Promise.all(responses.map(res => res.json()))
+  return Promise.all(urls.map(url => fetch(url)))
+    .then(
+      responses => Promise.all(responses.map(res => res.json())
+    ),
   );  
 }
