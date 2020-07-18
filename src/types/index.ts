0fc9376e4390e2c4ed057bc136cb8a88258ea848
Carlos Isaias Ruiz <zimoo354@gmail.com>
2020-07-18 18:38:56 -0500

    creating types

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+interface RepositoryType {
+  svn_url: string | null;
+  name: string | null;
+  description: string | null;
+};
+
+interface UserType {
+  name: string | null;
+  login: string | null;
+  html_url: string | null;
+  avatar_url: string | null;
+  bio: string | null;
+  location: string | null;
+  company: string | null;
+  blog: string | null;
+};
+
+export {
+  RepositoryType,
+  UserType,
+};
