89994601532b0321d76679644bf69eb2c22f4803
Carlos Isaias Ruiz <zimoo354@gmail.com>
2020-07-18 18:37:23 -0500

    code refactor to api functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,16 @@
 const baseUrl = "https://api.github.com";
 
-export const getUser = username => {
+export const getUser = async (username: string) => {
   const urls = [
     `${baseUrl}/users/${username}`,
     `${baseUrl}/users/${username}/repos`,
   ];
-  return Promise.all(urls.map(url => fetch(url)))
-    .then(
-      responses => Promise.all(responses.map(res => res.json())
-    ),
+  const responses = await Promise.all(urls.map(url => fetch(url)));
+    return await Promise.all(responses.map(res => {
+      if (res.status != 200)
+        return null;
+
+      return res.json();
+    })
   );  
 }
