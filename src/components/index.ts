c7955741d03d2d714d34858f6c2a7936a618bacf
Carlos Isaias Ruiz <zimoo354@gmail.com>
2020-07-18 18:38:32 -0500

    Components Index

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import RepositoryList from "./RepositoryList";
+import UserProfile from "./UserProfile";
+
+export {
+  Header,
+  RepositoryList,
+  UserProfile,
+};
